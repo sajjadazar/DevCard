f372768a8d946dcbcf23f9abc332db1bc87b515b
sajjadazar <azarbayjani96@gmail.com>
2022-02-14 13:28:04 +0330

    index / contact / links init

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -6,11 +6,11 @@ namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController()
         {
-            _logger = logger;
+    
         }
 
         public IActionResult Index()
@@ -18,7 +18,7 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
